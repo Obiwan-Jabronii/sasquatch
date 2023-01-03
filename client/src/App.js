11fea929a03cd61e56b2833fd28f7db0249fdb4c
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2023-01-03 14:23:12 -0500

    removed troublesome image, will have to work on it at another time

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src='/client/public/images/Sasquatch - Copy.xcf' alt='test'></img>
+      
       </header>
     </div>
   );
